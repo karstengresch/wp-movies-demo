3656085f71c561e67f8f4122ba197213c250cb2b
Mario Santos <santosguillamot@gmail.com>
2023-03-07 08:36:27 +0100

    Use node script

---
+++ .github/scripts/build-plugin.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const archiver = require('archiver');
+
+// Create a new zip file
+const output = fs.createWriteStream('wpmovies-plugin.zip');
+const archive = archiver('zip', { zlib: { level: 9 } });
+
+// Add files and directories to the zip file
+archive.directory('lib/', 'lib');
+archive.directory('src/', 'src');
+archive.directory('vendor/', 'vendor');
+archive.directory('build/', 'build');
+archive.file('README.md', { name: 'README.md' });
+archive.file('wpmovies.php', { name: 'wpmovies.php' });
+archive.file('.env', { name: '.env' });
+
+// Finalize the zip file
+archive.pipe(output);
+archive.finalize();
